587a959b5d5225220fadc4328d2bc9133965f007
Siripoj Sermkiatsakun <supershin2529@gmail.com>
2024-07-04 15:03:45 +0700

    first commit

---
+++ Project.Sanha.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project.Sanha.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
