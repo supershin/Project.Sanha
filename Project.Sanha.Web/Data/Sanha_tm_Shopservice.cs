7c87754394d25fd1dc53c2a2c165ee20b88ca8d5
Siripoj Sermkiatsakun <supershin2529@gmail.com>
2024-07-08 17:07:03 +0700

    gen db

---
+++ Project.Sanha.Web/Data/Sanha_tm_Shopservice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Sanha.Web.Data
+{
+    public partial class Sanha_tm_Shopservice
+    {
+        [Key]
+        public int ID { get; set; }
+        [StringLength(200)]
+        [Unicode(false)]
+        public string? Name { get; set; }
+        [StringLength(255)]
+        [Unicode(false)]
+        public string? Description { get; set; }
+        [StringLength(500)]
+        public string? LogoPath { get; set; }
+        public bool? FlagActive { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? CreateDate { get; set; }
+        public int? CreateBy { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? UpdateDate { get; set; }
+        public int? UpdateBy { get; set; }
+    }
+}
---
+++ Project.Sanha.Web/Data/Sanha_tr_ProjectShopservice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Sanha.Web.Data
+{
+    public partial class Sanha_tr_ProjectShopservice
+    {
+        [Key]
+        public int ID { get; set; }
+        [StringLength(20)]
+        [Unicode(false)]
+        public string ProjectID { get; set; } = null!;
+        public int ShopID { get; set; }
+        public int? Quata { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? DefaultStartDate { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? DefaultEndDate { get; set; }
+        public int? ExpireDate { get; set; }
+        public bool? FlagActive { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? CreateDate { get; set; }
+        public int? CreateBy { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? UpdateDate { get; set; }
+        public int? UpdateBy { get; set; }
+    }
+}
