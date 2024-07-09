63b161e62c6e8bb7815eb27de6cef0a8b614622e
Siripoj Sermkiatsakun <supershin2529@gmail.com>
2024-07-09 08:59:20 +0700

    scraffole db

---
+++ Project.Sanha.Web/Data/TitleDbContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Project.Sanha.Web.Data
+{
+    public partial class TitleDbContext : DbContext
+    {
+        public TitleDbContext()
+        {
+        }
+
+        public TitleDbContext(DbContextOptions<TitleDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Sanha_tm_Shopservice> Sanha_tm_Shopservice { get; set; } = null!;
+        public virtual DbSet<Sanha_tr_ProjectShopservice> Sanha_tr_ProjectShopservice { get; set; } = null!;
+        public virtual DbSet<Sanha_tr_Shopservice_Resource> Sanha_tr_Shopservice_Resource { get; set; } = null!;
+        public virtual DbSet<Sanha_tr_UnitShopservice> Sanha_tr_UnitShopservice { get; set; } = null!;
+        public virtual DbSet<Sanha_ts_Shopservice_Trans> Sanha_ts_Shopservice_Trans { get; set; } = null!;
+        public virtual DbSet<master_project> master_project { get; set; } = null!;
+        public virtual DbSet<master_unit> master_unit { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("Thai_CI_AS");
+
+            modelBuilder.Entity<Sanha_tm_Shopservice>(entity =>
+            {
+                entity.Property(e => e.CreateDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.FlagActive).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.UpdateDate).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Sanha_tr_ProjectShopservice>(entity =>
+            {
+                entity.Property(e => e.CreateDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.FlagActive).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.UpdateDate).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Sanha_tr_Shopservice_Resource>(entity =>
+            {
+                entity.Property(e => e.CreateDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.FlagActive).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.UpdateDate).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Sanha_tr_UnitShopservice>(entity =>
+            {
+                entity.Property(e => e.CreateDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.UpdateDate).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Sanha_ts_Shopservice_Trans>(entity =>
+            {
+                entity.Property(e => e.CreateDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.FlagActive).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.UpdateDate).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<master_project>(entity =>
+            {
+                entity.Property(e => e.id).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.Entity<master_unit>(entity =>
+            {
+                entity.Property(e => e.id).ValueGeneratedOnAdd();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
